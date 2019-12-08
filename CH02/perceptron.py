5abf17f194599cfb4091d71daa8f480a5af8d34c
bf2363 <bf2363@columbia.edu>
2019-12-08 13:32:32 -0500

    perceptron

---
+++ CH02/perceptron.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  8 13:31:59 2019
+
+@author: bowei
+
+
+    pd.options.display.float_format = '{:,.2f}'.format
+    pd.set_option('display.width', 3000)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.max_rows', 199)
+"""
+
